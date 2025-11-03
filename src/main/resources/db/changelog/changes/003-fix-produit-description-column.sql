a57213d4062f684c645b52a6bc5a6ae289a79483
lahcen <lahcenwirghanec11@gmail.com>
2025-11-03 10:29:14 +0100

    Fix liquibase issue

--- src/main/resources/db/changelog/changes/003-fix-produit-description-column.sql
+++
@@ -1,6 +0,0 @@
--- liquibase formatted sql
-
--- changeset Pood16:003-fix-produit-description-column
-ALTER TABLE produits MODIFY COLUMN description LONGTEXT;
-
--- rollback ALTER TABLE produits MODIFY COLUMN description TEXT;
