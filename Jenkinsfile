b721bf09fe57f3e23b37e3ed915edbcfe725d1cb
abdellatif guezadi <abdellatifguezadi@gmail.com>
2026-01-01 15:38:23 +0100

    feat: add test configuration and dependencies for H2 database in application-test.properties

--- Jenkinsfile
+++ Jenkinsfile
@@ -22,7 +22,7 @@ pipeline {
             }
             post {
                 always {
-                    publishTestResults testResultsPattern: 'target/surefire-reports/*.xml'
+                    junit testResultsPattern: 'target/surefire-reports/*.xml'
                 }
             }
         }
--- src/test/java/org/tricol/supplierchain/Brief6TricolSupplierChainApplicationTests.java
+++ src/test/java/org/tricol/supplierchain/Brief6TricolSupplierChainApplicationTests.java
@@ -2,8 +2,10 @@ package org.tricol.supplierchain;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class Brief6TricolSupplierChainApplicationTests {
 
     @Test
